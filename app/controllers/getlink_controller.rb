43dedc80b7de73223464b67fc6c6cb29b5e70f4b
Star Chow <pumastudio07@gmail.com>
2011-07-03 22:45:28 +0700

    add css, and error

--- app/controllers/getlink_controller.rb
+++ app/controllers/getlink_controller.rb
@@ -1,24 +1,35 @@
 require 'nokogiri'
 require 'open-uri'
 
+
+
 class GetlinkController < ApplicationController
   def index
 
   end
 
   def get_link
-    html = Nokogiri::HTML(open("#{params[:link]}"))
-    name = html.search("a.bigChar")
-    @name = name
-    @doc = []
-    html.search('table.listing a').each do |link|
-      @doc << link[:href] if link.content == "Download"
+    begin
+      html = Nokogiri::HTML(open("#{params[:link]}"))
+      name = html.search("a.bigChar")
+      @name = name
+      @doc = []
+      html.search('table.listing a').each do |link|
+        @doc << link[:href] if link.content == "Download"
+      end
+      @text = params[:link]
+    rescue Errno::ENOENT 
+      respond_to do |format|
+        format.js   { render 'error'}
+        format.xml  { render :xml => @mah_blogz }
+      end
+    else
+      respond_to do |format|
+        format.js
+        format.xml  { render :xml => @mah_blogz }
+      end
     end
-    @text = params[:link]
-    respond_to do |format|
-      format.js
-      format.xml  { render :xml => @mah_blogz }
-    end
+
   end
 
 end
